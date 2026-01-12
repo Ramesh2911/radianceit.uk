022a0d62a598689d31c50cbff77371aeca3a518a
Ramesh Jana <janaramesh15@gmail.com>
2026-01-12 20:52:35 +0530

    date range bold

--- src/components/leave/LeavesDetails.js
+++ src/components/leave/LeavesDetails.js
@@ -86,7 +86,7 @@ const LeavesDetails = (props) => {
             <div className="card-header d-flex justify-content-between align-items-center">
                <h5 className="mb-0">Leaves Details</h5>
 
-               <span className="text-muted fw-semibold">
+               <span className="fw-bold" style={{ color: "#000" }}>
                   {dateRange}
                </span>
             </div>
